50334363fbaa492a3c84b954791b4e3e91fef44a
Alex Miller <codex.nz@gmail.com>
2021-11-26 13:44:25 +1300

    Test if a non-error error is thrown.
    - Brings coverage to 100%.

--- lib/loader.spec.ts
+++ lib/loader.spec.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { Loader } from "cosmiconfig";
+import * as tsnode from "ts-node";
 import { TypeScriptLoader } from "./loader";
 import { TypeScriptCompileError } from "./typescript-compile-error";
 
@@ -37,5 +38,34 @@ describe("TypeScriptLoader", () => {
     } catch (error: unknown) {
       expect(error).toBeInstanceOf(TypeScriptCompileError);
     }
+  });
+
+  describe("ts-node", () => {
+    const unknownError = "Test Error";
+
+    let stub: jest.SpyInstance<tsnode.Service, [service: tsnode.Service]>;
+
+    beforeEach(() => {
+      stub = jest.spyOn(tsnode, "register").mockImplementation(() => {
+        // eslint-disable-next-line @typescript-eslint/no-throw-literal
+        throw unknownError;
+      });
+    });
+
+    afterEach(() => {
+      stub.mockRestore();
+    });
+
+    it("rethrows an error if it is not an instance of Error", () => {
+      try {
+        loader("filePath", "readFixtureContent(filePath)");
+        fail(
+          "Error should be thrown upon failing to transpile an invalid TS file."
+        );
+      } catch (error: unknown) {
+        expect(error).not.toBeInstanceOf(TypeScriptCompileError);
+        expect(error).toStrictEqual(unknownError);
+      }
+    });
   });
 });
