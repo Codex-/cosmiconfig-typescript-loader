263e64034bd4c53c418f830d50c27bbc618c6594
Alex Miller <codex.nz@gmail.com>
2021-11-30 10:12:39 +1300

    Fix error prototype chain

--- lib/typescript-compile-error.ts
+++ lib/typescript-compile-error.ts
@@ -2,6 +2,9 @@ export class TypeScriptCompileError extends Error {
   constructor(message: string) {
     super(message);
     this.name = this.constructor.name;
+
+    // https://github.com/Microsoft/TypeScript-wiki/blob/main/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 
   public static fromError(error: Error): TypeScriptCompileError {
